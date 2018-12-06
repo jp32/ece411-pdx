c8ad59f5e02bdb470a575ec9a915650519a74355
Bruno Duregon <bduregon@gmail.com>
2018-12-05 17:12:50 -0800

    Final code

--- ECE411_Code/ECE411_Code/ECE411_Code.ino
+++ ECE411_Code/ECE411_Code/ECE411_Code.ino
@@ -1,4 +1,4 @@
-/* ECE 411 Practicum Project - Jian Meng 
+/* ECE 411 Practicum Project - Jian Meng
    =========================
    Automatic Watering system
    =========================
@@ -7,12 +7,26 @@
 
 // Initialize the variables
 int soilpin = A5; // Declare a pin for the soil moisture sensor
+int UserSet = A4; // Declare a pin for the user to set water amount
 int Soilpower = 4; // Declare the power pin of the sensor
+int UserPower = 12; //Declare the power ping for user setting
 int Valvepower = 1;
 int count = 0;
-int M_UpperBound = 850;  // Upper bound of the moisture level
-int M_LowerBound = 757;  // Lower bound of the moisture level
+int M_UpperBound = 857;  // Upper bound of the moisture level
+int M_LowerBound = 750;  // Lower bound of the moisture level
 int sensorValue;         // The variable of the moisture value
+int UserValue;           // The variable of the user setting value
+
+
+void setup() {
+  // Initial Setup
+  Serial.begin(9600); // Serial port
+  pinMode(Soilpower, OUTPUT);  // Set the power pin of the sensor as the output
+  pinMode(Valvepower, OUTPUT); // Set the power pin of the valve as the output
+  digitalWrite(Valvepower, LOW); // Set to LOW so no power is flowing through the valve
+  digitalWrite(Soilpower, LOW); // Set to LOW so no power is flowing through the sensor
+
+}
 
 // Moisture Reading Function
 int readMoisture()
@@ -26,30 +40,24 @@ int readMoisture()
   return val;  // Return the moisture value
 }
 
+int readUserSetting()
+{
+  digitalWrite(UserPower, HIGH);  // Turn on voltage divider
+  delay(10); // Wait 10 milliseconds
+  int val2 = ((pow(analogRead(UserSet), 3) / 13000) - 1000); // Read the user valve open time setting non-linear
+  Serial.print("Valve open value = ");
+  Serial.println(val2);   // Print the value back to the serial port
+  digitalWrite(UserPower, LOW); // Turn off voltage divider
+  return val2;  // Return the moisture value
+}
+
 // Valve control function
 int valveControl()
 {
   digitalWrite(Valvepower, HIGH);
-  Serial.println("Valve is on");
-  for(count=0;count<10;count++)
-  {
-    // serial.print "Valve is on - 30 seconds - current time: print count - analg val(val)"
-    
-    delay(1000);
-  }
+  Serial.println("Valve is on, delaying...");
+  delay(UserValue);
 }
-
-
-void setup() {
-  // Initial Setup 
-  Serial.begin(9600); // Serial port
-  pinMode(Soilpower, OUTPUT);  // Set the power pin of the sensor as the output
-  pinMode(Valvepower, OUTPUT); // Set the power pin of the valve as the output
-  digitalWrite(Valvepower, LOW); // Set to LOW so no power is flowing through the valve
-  digitalWrite(Soilpower, LOW); // Set to LOW so no power is flowing through the sensor
-  
-}
-
 
 void loop() {
   // The loop will keep check the moisture level of the soil
@@ -57,11 +65,14 @@ void loop() {
   delay(2000);
   // serial print "done waiting two seconsd - reading moisture"
   sensorValue = readMoisture(); // Read the moisture level
-  if (sensorValue <= M_LowerBound){
-    Serial.print("Current moisture value = ");
+  UserValue   = readUserSetting(); //Read the user setting
+  if (sensorValue <= M_LowerBound && (sensorValue > 100)) {
+  Serial.print("Current moisture value = ");
     Serial.println(sensorValue);
+    Serial.print("Current user value = ");
+    Serial.println(UserValue);
     // If the moisture value lower than the lower bound, turn on the valve for 30 seconds
-    valveControl(); 
+    valveControl();
     digitalWrite(Valvepower, LOW); // Turn off the valve.
-    } 
+  }
 }
